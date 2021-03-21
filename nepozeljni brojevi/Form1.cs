9006a483711fc0d0f1d910f4909ff504983d7af6
Kaid22 <avdickaid@gmail.com>
2021-03-21 13:49:18 +0100

    komita

--- nepozeljni brojevi/Form1.cs
+++ nepozeljni brojevi/Form1.cs
@@ -16,5 +16,45 @@ namespace nepozeljni_brojevi
         {
             InitializeComponent();
         }
+        string temp;
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool gotovo = false;
+            string[] subs = textBox1.Text.Split(' ');
+            List<int> lista = new List<int>();
+            List<int> toremove = new List<int>();
+            foreach(string s in subs)
+            {
+                lista.Add(int.Parse(s));
+            }
+            while (gotovo==false)
+            {
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if (lista.Count % lista[i] == 0)
+                    {
+                        toremove.Add(lista[i]);
+                    }
+                }
+                foreach(int x in toremove)
+                {
+                    lista.Remove(x);
+                }
+                if (toremove.Count == 0)
+                {
+                    gotovo = true;
+                }
+                toremove.Clear();
+                
+                                                
+            }
+            label1.Text = null;
+            foreach(int x in lista)
+            {
+                temp += x.ToString() + " ";
+            }
+            label1.Text = temp;
+        }
     }
 }
